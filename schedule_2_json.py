a93186288ab31ebca3c3378576970e062cabec99
Camila Maia <cmaiacd@gmail.com>
2019-12-10 15:54:02 -0300

    Adicionado embed_link ao json

--- schedule_2_json.py
+++ schedule_2_json.py
@@ -17,6 +17,7 @@ def schedule_2_json():
     df = df[selected_headers]
     df.columns = ["datetime", "title", "duration", "link"]
     df["datetime"] = df["datetime"].apply(lambda x: convert_date(x))
+    df["embed_link"] = df["link"].apply(lambda x: convert_to_embed(x))
 
     df.to_json("output.json", orient="records", force_ascii=False)
 
@@ -24,6 +25,10 @@ def schedule_2_json():
 def convert_date(date_str):
     date = datetime.strptime(date_str, "%d/%m/%Y %H:%M:%S")
     return str(date)
+
+
+def convert_to_embed(url):
+    return url.replace("watch?v=", "embed/")
 
 
 if __name__ == "__main__":
